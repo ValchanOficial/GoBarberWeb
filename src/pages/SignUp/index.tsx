6c0e0166a0f054b15fe92399e13b77a4d74f29fe
Valéria Padilha de Vargas <valeria_padilha@yahoo.com.br>
2020-07-07 21:48:04 -0300

    chore: adicionando unform

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
+import { Form } from '@unform/web';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
@@ -9,12 +10,16 @@ import { ReactComponent as Logo } from '../../assets/logo.svg';
 import { Container, Content, Background } from './styles';
 
 const SignUp: React.FC = () => {
+  const handleSubmit = (): void => {
+    // console.log(data);
+  };
+
   return (
     <Container>
       <Background />
       <Content>
         <Logo title="GoBarber" />
-        <form>
+        <Form onSubmit={handleSubmit}>
           <h1>Faça seu cadastro</h1>
           <Input name="name" icon={FiUser} type="text" placeholder="Nome" />
           <Input name="email" icon={FiMail} type="email" placeholder="E-mail" />
@@ -25,7 +30,7 @@ const SignUp: React.FC = () => {
             placeholder="Senha"
           />
           <Button type="submit">Cadastrar</Button>
-        </form>
+        </Form>
         <a href="login">
           <FiArrowLeft />
           Voltar para logon
