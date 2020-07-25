3f1043a8b946bcce3165ba114ced9c1caa09cf7d
Valéria Padilha de Vargas <valeria_padilha@yahoo.com.br>
2020-07-25 19:28:15 -0300

    fix: husky eslint object as a type

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,11 +1,19 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import Tooltip from '../Tooltip';
 
-export const Container = styled.div`
+interface ContainerProps {
+  isFocused: boolean;
+  isFilled: boolean;
+  isErrored: boolean;
+}
+
+export const Container = styled.div<ContainerProps>`
   background: #232129;
   border-radius: 10px;
-  border: 2px solid #232129;
   padding: 16px;
   width: 100%;
+
+  border: 2px solid #232129;
   color: #666360;
 
   display: flex;
@@ -19,6 +27,25 @@ export const Container = styled.div`
     margin-right: 16px;
   }
 
+  ${({ isErrored }) =>
+    isErrored &&
+    css`
+      border-color: #c53030;
+    `}
+
+  ${({ isFocused }) =>
+    isFocused &&
+    css`
+      color: #ff9000;
+      border-color: #ff9000;
+    `}
+
+  ${({ isFilled }) =>
+    isFilled &&
+    css`
+      color: #ff9000;
+    `}
+
   input {
     flex: 1;
     background: transparent;
@@ -27,6 +54,24 @@ export const Container = styled.div`
 
     &::placeholder {
       color: #666360;
+    }
+  }
+`;
+
+export const Error = styled(Tooltip)`
+  height: 20px;
+  margin-left: 16px;
+
+  svg {
+    margin: 0;
+  }
+
+  span {
+    color: #fff;
+    background: #c53030;
+
+    &::before {
+      border-color: #c53030 transparent;
     }
   }
 `;
--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,5 +1,10 @@
-import React from 'react';
+import React, { useRef, useCallback } from 'react';
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
+import { FormHandles } from '@unform/core';
+import { Form } from '@unform/web';
+import * as Yup from 'yup';
+
+import getValidationErrors from '../../utils/getValidationErrors';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
@@ -8,14 +13,40 @@ import { ReactComponent as Logo } from '../../assets/logo.svg';
 
 import { Container, Content, Background } from './styles';
 
+interface Data {
+  email: string;
+  password: string;
+}
+
 const SignIn: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+
+  const handleSubmit = useCallback(async (data: Data) => {
+    try {
+      formRef.current?.setErrors({});
+      const schema = Yup.object().shape({
+        email: Yup.string()
+          .required('E-mail obrigatório')
+          .email('Digite um e-mail válido'),
+        password: Yup.string().required('Senha obrigatória'),
+      });
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+    } catch (err) {
+      const errors = getValidationErrors(err);
+      formRef.current?.setErrors(errors);
+    }
+  }, []);
+
   return (
     <Container>
       <Content>
         <Logo title="GoBarber" />
-        <form>
+
+        <Form ref={formRef} onSubmit={handleSubmit}>
           <h1>Faça seu logon</h1>
-          <Input name="email" icon={FiMail} type="email" placeholder="E-mail" />
+          <Input name="email" icon={FiMail} type="text" placeholder="E-mail" />
           <Input
             name="password"
             icon={FiLock}
@@ -24,7 +55,8 @@ const SignIn: React.FC = () => {
           />
           <Button type="submit">Entrar</Button>
           <a href="forgot">Esqueci minha senha</a>
-        </form>
+        </Form>
+
         <a href="login">
           <FiLogIn />
           Criar conta
